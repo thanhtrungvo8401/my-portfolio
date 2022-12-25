b2ecae9e04088b01128c9500d38b44a444d1e0dd
trungvt <thanhtrungvo8401@gmail.com>
2022-12-25 11:34:47 +0700

    update good reads

--- containers/good-reads/index.tsx
+++ containers/good-reads/index.tsx
@@ -7,7 +7,7 @@ export default function GoodReads() {
     <Container>
       <Headline1 className="mt-8">Good reads</Headline1>
 
-      <div className="flex p-4 shadow">
+      <div className="flex gap-4 p-4 shadow">
         <a
           target="_blank"
           href="https://levelup.gitconnected.com/ultimate-10-stages-to-master-backend-development-f6f65f22a327"
@@ -16,6 +16,22 @@ export default function GoodReads() {
           <Button className="!font-normal">
             Ultimate 10 Stages to Master Backend Development
           </Button>
+        </a>
+
+        <a
+          target="_blank"
+          href="https://khalilstemmler.com/blogs/typescript/eslint-for-typescript/"
+          rel="noreferrer"
+        >
+          <Button className="!font-normal">Eslint-Prettier (P1)</Button>
+        </a>
+
+        <a
+          target="_blank"
+          href="https://khalilstemmler.com/blogs/tooling/prettier/"
+          rel="noreferrer"
+        >
+          <Button className="!font-normal">Eslint-Prettier (P2)</Button>
         </a>
       </div>
     </Container>
